47baebefe226d18229eb728c075a684086785441
Noman Gazi <numan.islam61@gmail.com>
2023-02-10 20:17:15 +0600

    Initialize project using Create React App

---
+++ state/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NoteTakingApp from './components/NoteTakingApp'
+
+function App() {
+    return (
+        <div className='py-10 h-screen'>
+            <div className='max-w-6xl mx-auto'>
+                <NoteTakingApp />
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ state/src/NoPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoPage = () => {
+    return <div className='text-center text-5xl'>404-Not Found</div>;
+};
+
+export default NoPage;
